827396eabb21e36d8208c475b73337e46806478e
Allan F Ribeiro <allanfribeiro@gmail.com>
2021-11-06 15:13:15 -0300

    Saldo Médio Crédito

---
+++ saldoMedio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace saldoMedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double saldo, credito;
+
+			Console.WriteLine("Informe seu último saldo");
+			saldo = double.Parse(Console.ReadLine());
+
+			if (saldo <= 200)
+			{
+				credito = 0;
+			}
+			else if (saldo <= 400)
+			{
+				credito = saldo * 0.2;
+			}
+			else if (saldo <= 600)
+			{
+				credito = saldo *  0.3;
+			}
+			else
+			{
+				credito = saldo *  0.4;
+			}
+
+			Console.WriteLine("Seu saldo médio é R${0} e crédito de R${1}", saldo.ToString("F2"), credito.ToString("F2"));
+		}
+    }
+}
